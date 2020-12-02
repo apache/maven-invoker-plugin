bfd740140051e757ab44c516138c9d4263541680
Slawomir Jaranowski <s.jaranowski@gmail.com>
2020-12-02 01:13:16 +0100

    [MINVOKER-260] confirm streamLogs by IT tests
    also resolve: [MINVOKER-265]

---
+++ src/it/script-streamLogs-false/src/it/groovy/setup.groovy
@@ -0,0 +1,30 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.*;
+
+// marker for parent build that this sub build was indeed run
+File touchFile = new File( basedir, "touch.txt" )
+touchFile.createNewFile()
+
+println("Output from groovy script")
+
+def pom = new File( basedir, "pom.xml" ).text
+assert pom.contains("<modelVersion>9.9.9</modelVersion>")
+
