9827386095ffc10f69fef49c19f90dad3bd3867b
Slawomir Jaranowski <s.jaranowski@gmail.com>
2021-12-02 22:37:18 +0100

    [MINVOKER-287] Group project by ordinal in parallel mode
    Close #80

---
+++ src/main/java/org/apache/maven/plugins/invoker/JobExecutor.java
@@ -0,0 +1,93 @@
+package org.apache.maven.plugins.invoker;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import org.apache.maven.plugins.invoker.model.BuildJob;
+
+/**
+ * Execute build jobs with parallel.
+ *
+ * @author Slawomir Jaranowski
+ */
+class JobExecutor
+{
+    interface ThrowableJobConsumer
+    {
+        void accept( BuildJob t ) throws Throwable;
+    }
+
+    private final List<BuildJob> jobs;
+    private final int threadsCount;
+
+    JobExecutor( List<BuildJob> jobs, int threadsCount )
+    {
+        this.jobs = jobs;
+        this.threadsCount = threadsCount;
+    }
+
+    public void forEach( ThrowableJobConsumer jobConsumer )
+    {
+        // group and sort jobs by ordinal
+        Map<Integer, List<BuildJob>> groupedJobs = jobs.stream()
+            .sorted( ( j1, j2 ) -> Integer.compare( j2.getOrdinal(), j1.getOrdinal() ) )
+            .collect( Collectors.groupingBy( BuildJob::getOrdinal, LinkedHashMap::new, Collectors.toList() ) );
+
+        ExecutorService executorService = Executors.newFixedThreadPool( threadsCount );
+
+        groupedJobs.forEach( ( key, value ) ->
+        {
+            // prepare list of callable tasks
+            List<Callable<Void>> callableJobs = value.stream().map( buildJob -> (Callable<Void>) () ->
+            {
+                try
+                {
+                    jobConsumer.accept( buildJob );
+                }
+                catch ( Throwable e )
+                {
+                    buildJob.setResult( BuildJob.Result.ERROR );
+                    buildJob.setFailureMessage( String.valueOf( e ) );
+                }
+                return null;
+            } ).collect( Collectors.toList() );
+
+            try
+            {
+                executorService.invokeAll( callableJobs );
+            }
+            catch ( InterruptedException e )
+            {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException( e );
+            }
+        } );
+
+        // all task are finished here
+        executorService.shutdownNow();
+    }
+}
---
+++ src/test/java/org/apache/maven/plugins/invoker/JobExecutorTest.java
@@ -0,0 +1,157 @@
+package org.apache.maven.plugins.invoker;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.maven.plugins.invoker.model.BuildJob;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+/**
+ * Tests for JobExecutor.
+ *
+ * @author Slawomir Jaranowski
+ */
+public class JobExecutorTest
+{
+
+    @Test
+    public void emptyJobList()
+    {
+        JobExecutor jobExecutor = new JobExecutor( Collections.emptyList(), 1 );
+
+        jobExecutor.forEach( job -> fail( "fail" ) );
+    }
+
+    @Test
+    public void failedJob()
+    {
+        BuildJob job = aJob( "job1", 100 );
+
+        JobExecutor jobExecutor = new JobExecutor( Collections.singletonList( job ), 1 );
+
+        jobExecutor.forEach( j -> fail( "fail " + j.getProject() ) );
+
+        assertThat( job.getResult() ).isEqualTo( BuildJob.Result.ERROR );
+        assertThat( job.getFailureMessage() ).isEqualTo( "java.lang.AssertionError: fail job1" );
+    }
+
+    @Test
+    public void jobsShouldBeGroupedAndExecutedInProperOrder()
+    {
+        Map<Integer, AtomicInteger> jobsCounter = new HashMap<>();
+        jobsCounter.put( 100, new AtomicInteger( 3 ) );
+        jobsCounter.put( 10, new AtomicInteger( 2 ) );
+        jobsCounter.put( 1, new AtomicInteger( 1 ) );
+
+        BuildJob job1 = aJob( "job1-100", 100 );
+        BuildJob job2 = aJob( "job2-100", 100 );
+        BuildJob job3 = aJob( "job3-100", 100 );
+
+        BuildJob job4 = aJob( "job4-10", 10 );
+        BuildJob job5 = aJob( "job5-10", 10 );
+
+        BuildJob job6 = aJob( "job6-1", 1 );
+
+        // put jobs in wrong order
+        List<BuildJob> jobs = Arrays.asList(
+            job4, job5,
+            job1, job2,
+            job6,
+            job3
+        );
+
+        JobExecutor jobExecutor = new JobExecutor( jobs, 10 );
+
+        jobExecutor.forEach( job ->
+        {
+            jobsCounter.get( job.getOrdinal() ).decrementAndGet();
+
+            switch ( job.getOrdinal() )
+            {
+                case 100:
+                    assertThat( jobsCounter.get( 10 ).get() )
+                        .as( "Jobs-10 must not be executed before 100" )
+                        .isEqualTo( 2 );
+
+                    assertThat( jobsCounter.get( 1 ).get() )
+                        .as( "Jobs-1 must not be executed before 100" )
+                        .isEqualTo( 1 );
+                    break;
+
+                case 10:
+                    assertThat( jobsCounter.get( 100 ).get() )
+                        .as( "Jobs-100 must be executed before 10" )
+                        .isZero();
+
+                    assertThat( jobsCounter.get( 1 ).get() )
+                        .as( "Jobs-1 must not be executed before 10" )
+                        .isEqualTo( 1 );
+                    break;
+
+                case 1:
+                    assertThat( jobsCounter.get( 100 ).get() )
+                        .as( "Jobs-100 must be executed before 1" )
+                        .isZero();
+
+                    assertThat( jobsCounter.get( 10 ).get() )
+                        .as( "Jobs-10 must be executed before 1" )
+                        .isZero();
+                    break;
+
+                default:
+                    fail( "invalid job ordinal value %d", job.getOrdinal() );
+                    break;
+            }
+
+            job.setResult( BuildJob.Result.SUCCESS );
+            job.setDescription( Thread.currentThread().getName() );
+        } );
+
+        // all jobs have success status
+        assertThat( jobs ).allSatisfy( job ->
+        {
+            assertThat( job.getDescription() ).isNotBlank();
+            assertThat( job.getResult() ).as( job.getFailureMessage() ).isEqualTo( BuildJob.Result.SUCCESS );
+        } );
+
+        // jobs run on separate thread
+        assertThat( job1.getDescription() ).isNotEqualTo( job2.getDescription() );
+        assertThat( job1.getDescription() ).isNotEqualTo( job3.getDescription() );
+        assertThat( job2.getDescription() ).isNotEqualTo( job3.getDescription() );
+
+        assertThat( job4.getDescription() ).isNotEqualTo( job5.getDescription() );
+    }
+
+    private BuildJob aJob( String name, int ordinal )
+    {
+        BuildJob buildJob = new BuildJob( name );
+        buildJob.setOrdinal( ordinal );
+        return buildJob;
+    }
+}
